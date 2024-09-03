231915e54894b2762e0eb2b4e42829fd1b23779a
Fleege <76089935+pfleege@users.noreply.github.com>
2024-09-03 15:21:11 +0300

    ADD group element to bar-chart in order to move it
    - Use translate to move the group -element by margin.left + margin-top

--- src/App.jsx
+++ src/App.jsx
@@ -39,16 +39,18 @@ function App() {
 
   return (
     <svg height={height} width={width}>
-      {data.map((d, i) => (
-        <rect
-          key={i}
-          x={0}
-          y={yScale(d.Country)}
-          width={xScale(d.Population)}
-          height={yScale.bandwidth()}
-          fill={"white"}
-        />
-      ))}
+      <g transform={`translate(${margin.left}, ${margin.top})`}>
+        {data.map((d, i) => (
+          <rect
+            key={i}
+            x={0}
+            y={yScale(d.Country)}
+            width={xScale(d.Population)}
+            height={yScale.bandwidth()}
+            fill={"white"}
+          />
+        ))}
+      </g>
     </svg>
   );
 }
