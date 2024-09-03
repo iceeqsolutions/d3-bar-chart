753057c662a3aecb0219d01205e80fee709b6664
Fleege <76089935+pfleege@users.noreply.github.com>
2024-09-03 14:40:20 +0300

    ADD initial structure to bar chart (rect) -element
    - rect -element require 4 attributes: x, y, width and height
    - Our x-attribute will ot have a value
    - Our x-attribute will be the countries, which we acquire
    from the data by mappin through the "Country" -column

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import "./App.css";
 import { useEffect, useState } from "react";
-import { csv } from "d3";
+import { csv, scaleBand } from "d3";
 
 const csvUrl =
   "https://gist.githubusercontent.com/curran/0ac4077c7fc6390f5dd33bf5c06cb5ff/raw/605c54080c7a93a417a3cea93fd52e7550e76500/UN_Population_2019.csv";
+
+const width = 960;
+const height = 500;
 
 function App() {
   const [data, setData] = useState(null);
@@ -16,10 +19,14 @@ function App() {
     return <pre>Loading data...</pre>;
   }
 
+  const yScale = scaleBand()
+    .domain(data.map(d => d.Country))
+    .range([0, height]);
+    
   return (
-    <>
-      <pre>Data available!</pre>
-    </>
+    <svg>
+      {data.map(d => <rect x={0} y={yScale(d.Country)} width={} height={}/>)}
+    </svg>
   );
 }
 
